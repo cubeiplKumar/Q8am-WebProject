ffd589c0bc44ca0e90a83b71d6773c66a3ef21a1
cubeiplkumar <trainings.anil@gmail.com>
2021-08-21 08:39:42 +0530

    feature 301 changes by mahesh

--- file1.java
+++ file1.java
@@ -21,4 +21,10 @@ public class SwapNumbers {
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
     }
+	//feature 301 changes by mahesh
+	public static void sampleMethod(int a, int b) throws ArithmeticException{
+        System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   } 
 }
